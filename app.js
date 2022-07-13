eca32f15e4e17a4f29b811bc37515992131bf80c
Pratik Somwanshi <pratiksomwanshi570@gmail.com>
2022-07-13 14:44:56 +0530

    done with promises

--- app.js
+++ app.js
@@ -1,32 +1,38 @@
 // using xml
-const getToodos = (callback) => {
-    const request = new XMLHttpRequest();
+const getToodos = (resource) => {
+    return new Promise((resolve, reject) => {
+        const request = new XMLHttpRequest();
 
-    request.addEventListener("readystatechange", () => {
-        // console.log(request, request.readyState);
-        if (request.status === 200 && request.readyState === 4) {
-            const data = JSON.parse(request.responseText);
-            callback(undefined, data);
-        } else if (request.readyState === 4) {
-            callback("could not fetch data", undefined);
-        }
+        request.addEventListener("readystatechange", () => {
+            // console.log(request, request.readyState);
+            if (request.status === 200 && request.readyState === 4) {
+                const data = JSON.parse(request.responseText);
+                resolve(data);
+            } else if (request.readyState === 4) {
+                reject("could not fetch data");
+            }
+        });
+
+        request.open("GET", resource);
+
+        request.send();
     });
-
-    request.open("GET", "https://dog.ceo/api/breeds/image/random");
-
-    request.send();
 };
 
-console.log(1);
-console.log(2);
+// promises example
 
-getToodos((err, data) => {
-    if (err) {
-        console.log(err);
-    } else {
-        console.log(data);
-    }
-});
-
-console.log(3);
-console.log(4);
+getToodos("todos/first.json")
+    .then((data) => {
+        console.log("Promise resolved 1", data);
+        return getToodos("todoss/second.json");
+    })
+    .then((data) => {
+        console.log("Promise resolved 2", data);
+        return getToodos("todos/third.json");
+    })
+    .then((data) => {
+        console.log("Promise resolved 3", data);
+    })
+    .catch((err) => {
+        console.log("error occured");
+    });
